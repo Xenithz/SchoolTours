145aacbadf32d13d35355a9a69ec8dc758169f30
Angelo G <Angelo@Aneeshas-MacBook-Pro-3.local>
2018-06-13 13:02:07 +0400

    test

---
+++ TestProject/Assets/Low Poly Cartoon Forest Pack/readme.txt
@@ -0,0 +1,39 @@
+﻿Low Poly Cartoon Forest Pack contains many fantastic, cartoon-style forest elements.
+
+Models
+- bough
+- grass
+- mushrooms (4 types)
+- roots
+- stones (4 types)
+- trees (6 types)
+- trunk
+- water lily
+- wood
+
+Textures of all models are on single atlas (normal map included).
+There is also version of atlas with transparency for trees.
+
+Textures for terrain
+- grass + normal map (4 types)
+- ground + normal map
+- road + normal map
+- rock + normal map
+- water
+
+Particles
+- butterflies (3 types)
+- lightbeam
+
+Contact us at: multidarek@gmail.com
+
+------------------------------------------
+Changelog
+
+version 1.1
+
+- added texture atlas with transparency for trees
+- added prefabs for trees using atlas with transparency
+- added 2 new stones (Stones3, Stones4)
+- added 2 new trees (Spruce4, Spruce5)
+- added new terrain texture road + normal map
---
+++ TestProject/Assets/MyScripts/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using Vuforia;
+
+public class Test : MonoBehaviour
+{
+    public GameObject myPanel;
+    public VideoPlayer myVideo;
+    public RawImage myImage;
+
+    public void TextButton()
+    {
+        if(myPanel.activeInHierarchy != true)
+        {
+            myPanel.SetActive(true);
+            StartCoroutine(StartVideo());
+        }
+        else
+        {
+            myPanel.SetActive(false);
+            StopCoroutine(StartVideo());
+        }
+    }
+
+    public IEnumerator StartVideo()
+    {
+        myVideo.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while(!myVideo.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        myImage.texture = myVideo.texture;
+    }
+}
