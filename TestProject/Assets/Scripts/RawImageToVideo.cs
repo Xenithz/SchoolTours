8a0e590e45ce2a4e81615ec8255592d7e660d461
Angelo G <Angelo@Aneeshas-MacBook-Pro-3.local>
2018-06-18 12:35:41 +0400

    more stuff

---
+++ TestProject/Assets/Scripts/RawImageToVideo.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class RawImageToVideo : MonoBehaviour 
+{
+	[SerializeField]
+	private RawImage myImage;
+
+	[SerializeField]
+	private VideoClip myVideo;
+
+	[SerializeField]
+	private VideoPlayer myVideoPlayer;
+
+	[SerializeField]
+	private VideoSource myVideoSource;
+
+	[SerializeField]
+	private AudioSource myAudioSource;
+
+	private void Start()
+	{
+		Application.runInBackground = true;
+		StartCoroutine(StartVideo());
+	}
+
+	IEnumerator StartVideo()
+	{
+		myVideoPlayer = gameObject.AddComponent<VideoPlayer>();
+		myAudioSource = gameObject.AddComponent<AudioSource>();
+
+		myVideoPlayer.playOnAwake = false;
+		myAudioSource.playOnAwake = false;
+		myAudioSource.Pause();
+		
+		myVideoPlayer.source = VideoSource.VideoClip;
+
+		myVideoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+		myVideoPlayer.EnableAudioTrack(0, true);
+		myVideoPlayer.SetTargetAudioSource(0, myAudioSource);
+
+		myVideoPlayer.clip = myVideo;
+		myVideoPlayer.Prepare();
+
+		while(myVideoPlayer.isPrepared == false)
+		{
+			yield return null;
+		}
+
+		myImage.texture = myVideoPlayer.texture;
+
+		myVideoPlayer.Play();
+
+		myAudioSource.Play();
+
+		while(myVideoPlayer.isPlaying == true)
+		{
+			Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)myVideoPlayer.time));
+			yield return null;
+		}
+	}
+}
