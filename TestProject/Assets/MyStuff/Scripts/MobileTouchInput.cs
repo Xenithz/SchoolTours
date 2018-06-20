218f8dbdd95ea44124a09ccd5ca43e9d335bc5a0
Angelo G <Angelo@Aneeshas-MacBook-Pro-3.local>
2018-06-20 14:40:15 +0400

    more more stuff

---
+++ TestProject/Assets/MyStuff/Scripts/MobileTouchInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileTouchInput : MonoBehaviour 
+{
+	private RaycastHit hit;
+
+	private void Update()
+	{
+	}
+
+	private void ProcessInput()
+	{
+		foreach(Touch myTouchReg in Input.touches)
+		{
+			Debug.Log("test");
+			if(myTouchReg.phase == TouchPhase.Began)
+			{
+				Ray myRay = Camera.main.ScreenPointToRay(myTouchReg.position);
+
+				if(Physics.Raycast(myRay, out hit))
+				{
+					Execute(hit);
+				}
+			}
+		}
+	}
+
+	private void Execute(RaycastHit hitToCheck)
+	{
+		switch (hitToCheck.transform.gameObject.tag)
+		{
+			case "Test":
+				Debug.Log("Hit");
+				break;
+			default:
+				break;
+		}
+	}
+}
---
+++ TestProject/Assets/MyStuff/Scripts/TouchInputTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInputTest : MonoBehaviour 
+{
+	public GameObject objectToCreate;
+	private RaycastHit hit;
+
+	private void Update()
+	{
+		foreach(Touch myTouchReg in Input.touches)
+		{
+			Debug.Log("test");
+			if(myTouchReg.phase == TouchPhase.Began)
+			{
+				Ray myRay = Camera.main.ScreenPointToRay(myTouchReg.position);
+
+				if(Physics.Raycast(myRay, out hit))
+				{
+					Instantiate(objectToCreate, hit.point, Quaternion.identity);
+				}
+			}
+		}
+	}
+}
