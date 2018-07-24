321a55e3dd8bad99ce7294bef864444823a47e55
Angelo G <Angelo@Aneeshas-MacBook-Pro-3.local>
2018-07-24 10:12:21 +0400

    imported textures and stuff

---
+++ TestProject/Assets/WallVideo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallVideo : MonoBehaviour 
+{
+	private MovieTexture myMovieTexture;
+
+	private void Start()
+	{
+		
+	}
+}
